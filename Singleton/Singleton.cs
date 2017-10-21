de247edf7b6ad6b712ca7fcb42d919fb8d6208b6
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2017-10-21 11:06:19 +0200

    singleton

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private static readonly object Lock = new Object();
+
+        private Singleton()
+        {
+            
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Singleton();
+                        }
+
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
