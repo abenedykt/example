331b37c58945db447e5d891066ef1ff1de142d3c
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2017-10-21 12:36:04 +0200

    factory example

---
+++ FactoryExample/Class1.cs
@@ -0,0 +1,70 @@
+﻿namespace FactoryExample
+{
+
+    public class Factory
+    {
+        public Klient CreateClient()
+        {
+            return new Klient();
+        }
+
+        public Zamowienie CreateZamowienie()
+        {
+            return new Zamowienie(new Klient());
+        }
+
+
+        public Word CreateWord()
+        {
+            return new Document
+            {
+                new Page
+                {
+                    new Paragraph
+                    {
+                        new Line(),
+                        new Line(),
+                        new Line(),
+                        new Line(),
+                        new Line(),
+                    }
+                },
+                new Page
+                {
+                    new Paragraph
+                    {
+                        new Line(),
+                        new Line(),
+                        new Line(),
+                        new Line(),
+                        new Line(),
+                    }
+                }
+            }
+        }
+    }
+
+    public class Klient
+    {
+        public void DodajZamowienie(Zamowienie zamowienie)
+        {
+        }
+    }
+
+    public class Zamowienie
+    {
+        public Klient Klient { get; set; }
+    }
+
+    public class Example
+    {
+        public void Example()
+        {
+            var klient1 = new Klient();
+
+            klient1.DodajZamowienie(new Zamowienie());
+
+        }
+    }
+}
+
