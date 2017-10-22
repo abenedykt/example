e98d28037826739235ec70514217d07cf766853b
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2017-10-22 12:02:14 +0200

    added nsubstitue

---
+++ Food.Tests/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Food.Abstract;
+using Food.Commands;
+using NSubstitute;
+using Xunit;
+
+namespace Food.Tests
+{
+    public class CommandEmptyBasketTests
+    {
+        [Fact]
+        public void Command_should_execute_clear_on_basket()
+        {
+            var basket = Substitute.For<IBasket>();
+            var command = new CommandEmptyBasket(basket);
+
+            command.Execute();
+
+            basket.Received(1).Clear();
+        }
+    }
+
+    public class CommandAddMenuItemTests
+    {
+        [Fact]
+        public void TestName()
+        {
+            //var command = 
+        }
+    }
+
+
+}
--- Food.Tests/Properties/AssemblyInfo.cs
+++ Food.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jedzenie")]
+[assembly: AssemblyTitle("Food.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jedzenie")]
+[assembly: AssemblyProduct("Food.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("69d4b75a-5680-45fc-b5f6-a2dad84bbfc5")]
+[assembly: Guid("b09e2e42-2b1a-46eb-ac99-90049dff1b93")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("Food.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
